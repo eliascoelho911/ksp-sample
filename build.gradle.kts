9d682aa9cf7441e612f382a7ab08e7536e3625cd
elias <elias_coelho911@hotmail.com>
2023-08-22 11:18:22 -0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("com.android.application") version "8.0.0-beta01" apply false
+    id("com.android.library") version "8.0.0-beta01" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("com.google.devtools.ksp") version "1.8.0-1.0.8"
+    kotlin("jvm") version "1.8.0"
+}
+
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+    }
+}
